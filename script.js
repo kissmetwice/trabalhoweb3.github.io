0f0dd1b318915e6e4d885b20ed5571d384b7bdac
｡:ﾟ(;´∩`;)ﾟ:｡ <105591886+kissmetwice@users.noreply.github.com>
2023-06-08 11:21:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+function alterarCor1() {
+  const stt = document.getElementById('sttArm1');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor2() {
+  const stt = document.getElementById('sttArm2');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor3() {
+  const stt = document.getElementById('sttArm3');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor4() {
+  const stt = document.getElementById('sttArm4');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor5() {
+  const stt = document.getElementById('sttArm5');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor6() {
+  const stt = document.getElementById('sttArm6');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor7() {
+  const stt = document.getElementById('sttArm7');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor8() {
+  const stt = document.getElementById('sttArm8');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor9() {
+  const stt = document.getElementById('sttArm9');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor10() {
+  const stt = document.getElementById('sttArm10');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor11() {
+  const stt = document.getElementById('sttArm11');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor12() {
+  const stt = document.getElementById('sttArm12');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+function alterarCor13() {
+  const stt = document.getElementById('sttArm13');
+  if (stt.style.backgroundColor === 'lightgreen') {
+    stt.style.backgroundColor = 'lightcoral';
+  } else {
+    stt.style.backgroundColor = 'lightgreen';
+  }
+}
+
+
+
+
